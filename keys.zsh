9f2ea0c1f738ceeb1e7b413ae83aca5b2e088429
Allan Larangeiras <alarangeiras@gmail.com>
2024-09-22 16:35:20 -0300

    feat: add execution time

--- keys.zsh
+++ keys.zsh
@@ -1,6 +1,26 @@
+# add dir auto completion
 zstyle ":completion:*" special-dirs true
 autoload -Uz compinit
 compinit
 
+# add execution time
+function pre_exec() {
+  timer=${timer:-$SECONDS}
+}
+
+function pre_cmd() {
+  if [ $timer ]; then
+    timer_show=$(($SECONDS - $timer))
+    export RPROMPT="%F{cyan}${timer_show}s %{$reset_color%}"
+    unset timer
+  fi
+}
+
+zmodload zsh/datetime
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd pre_cmd
+add-zsh-hook preexec pre_exec
+
+# change navigation keys
 bindkey "^[[1;5C" forward-word # ctrl + arrow right
 bindkey "^[[1;5D" backward-word # ctrl + arrow right
