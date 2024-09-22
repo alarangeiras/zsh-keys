b1d8b6b39b9c13e046738f83fde13b4ec36d6f21
Allan Larangeiras <alarangeiras@gmail.com>
2024-09-22 15:27:28 -0300

    feat: add auto complete

--- keys.zsh
+++ keys.zsh
@@ -1,2 +1,6 @@
+zstyle ":completion:*" special-dirs true
+autoload -Uz compinit
+compinit
+
 bindkey "^[[1;5C" forward-word # ctrl + arrow right
 bindkey "^[[1;5D" backward-word # ctrl + arrow right
